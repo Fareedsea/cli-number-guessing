fc202ed4f878a5c914b1b9f40ed0c9cf40acb3af
Fareedsea <fareed.sea@gmail.com>
2024-03-23 14:43:50 +0500

    Add New Files

---
+++ index.ts
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 +1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a Number: ",
+},
+]);
+
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratualation you guessed the right number");
+} else {
+    console.log("You guessed the wrong number");
+}
+
+
