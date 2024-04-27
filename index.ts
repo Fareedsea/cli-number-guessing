3e4069cb8372288423c9817416189fa8254ffe76
Fareedsea <fareed.sea@gmail.com>
2024-04-27 12:13:26 +0500

    update files

--- index.ts
+++ index.ts
@@ -4,17 +4,17 @@ import chalk from "chalk";
 
 const randomNumber = Math.floor(Math.random() * 10 + 1);
 const answers = await inquirer.prompt([
-    {
-        name: "userGuessedNumber",
-        type: "number",
-        message: "Please guess a Number from 1-10 : ",
-},
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please guess a Number from 1-10 : ",
+  },
 ]);
 
 if (answers.userGuessedNumber === randomNumber) {
-    console.log(chalk.white.bgGreen("Congratualation you guessed the right number"));
+  console.log(
+    chalk.white.bgGreen("Congratualation you guessed the right number")
+  );
 } else {
-    console.log(chalk.white.bgRed("You guessed the wrong number"));
+  console.log(chalk.white.bgRed("You guessed the wrong number"));
 }
-
-
